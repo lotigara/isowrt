23dbc595e94d037abafb8324fd1523a1150a4099
lotigara <lotigara@yandex.ru>
2023-12-16 13:46:25 +0300

    fixed installer

--- install.sh
+++ install.sh
@@ -19,10 +19,10 @@
 copy () {
 	chmod +x isowrt.sh
 	if [[ $1 == "system" ]] ; then
-		sudo cp isowrt.sh /usr/bin/local
+		sudo cp isowrt.sh /usr/bin/local/isowrt
 		echo 'Note that you need to add /usr/bin/local to your $PATH in your shell config file (e.g. .bashrc or .zshrc. Usually it already added, but you have to check it!'
 	elif [[ $1 == "user" ]] ; then
-		cp isowrt.sh ~/.local/bin/
+		cp isowrt.sh ~/.local/bin/isowrt
 		echo 'Note that you need to add $HOME/.local/bin to your $PATH in your shell config file (e.g. .bashrc or .zshrc. Usually it already added, but you have to check it!'
 	else
 		echo 'invalid installation type'
